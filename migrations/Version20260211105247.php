be5ce72c9e10369517fc5e57860456af3124ba35
diakite <modidiakite@hotmail.fr>
2026-02-11 15:21:10 +0100

    correctif fichier du projet

---
+++ migrations/Version20260211105247.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260211105247 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE equipement DROP FOREIGN KEY `FK_B8B4C6F39E6B1585`');
+        $this->addSql('ALTER TABLE equipement DROP FOREIGN KEY `FK_B8B4C6F3F6BD1646`');
+        $this->addSql('ALTER TABLE intervention DROP FOREIGN KEY `FK_D11814AB427EB8A5`');
+        $this->addSql('ALTER TABLE intervention DROP FOREIGN KEY `FK_D11814AB896DBBDE`');
+        $this->addSql('ALTER TABLE intervention DROP FOREIGN KEY `FK_D11814AB9E6B1585`');
+        $this->addSql('ALTER TABLE intervention DROP FOREIGN KEY `FK_D11814ABB03A8386`');
+        $this->addSql('ALTER TABLE intervention DROP FOREIGN KEY `FK_D11814ABE6C5D496`');
+        $this->addSql('ALTER TABLE request DROP FOREIGN KEY `FK_3B978F9F806F0F5C`');
+        $this->addSql('ALTER TABLE request DROP FOREIGN KEY `FK_3B978F9F896DBBDE`');
+        $this->addSql('ALTER TABLE request DROP FOREIGN KEY `FK_3B978F9F9E6B1585`');
+        $this->addSql('ALTER TABLE request DROP FOREIGN KEY `FK_3B978F9FB03A8386`');
+        $this->addSql('ALTER TABLE request DROP FOREIGN KEY `FK_3B978F9FF6BD1646`');
+        $this->addSql('ALTER TABLE site DROP FOREIGN KEY `FK_694309E49E6B1585`');
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY `FK_8D93D6499E6B1585`');
+        $this->addSql('DROP TABLE equipement');
+        $this->addSql('DROP TABLE intervention');
+        $this->addSql('DROP TABLE request');
+        $this->addSql('DROP TABLE site');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('ALTER TABLE organisation ADD actif TINYINT NOT NULL, CHANGE name nom VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE equipement (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, brand VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, model VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, serial_number VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, status VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, organisation_id INT DEFAULT NULL, site_id INT DEFAULT NULL, INDEX IDX_B8B4C6F39E6B1585 (organisation_id), INDEX IDX_B8B4C6F3F6BD1646 (site_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE intervention (id INT AUTO_INCREMENT NOT NULL, plan_date DATETIME NOT NULL, report LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, status VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, organisation_id INT DEFAULT NULL, request_id INT DEFAULT NULL, technician_id INT DEFAULT NULL, created_by_id INT DEFAULT NULL, updated_by_id INT DEFAULT NULL, INDEX IDX_D11814AB9E6B1585 (organisation_id), INDEX IDX_D11814AB427EB8A5 (request_id), INDEX IDX_D11814ABE6C5D496 (technician_id), INDEX IDX_D11814ABB03A8386 (created_by_id), INDEX IDX_D11814AB896DBBDE (updated_by_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE request (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, description LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, priority VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, status VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, organisation_id INT DEFAULT NULL, site_id INT DEFAULT NULL, equipement_id INT DEFAULT NULL, created_by_id INT DEFAULT NULL, updated_by_id INT DEFAULT NULL, INDEX IDX_3B978F9F9E6B1585 (organisation_id), INDEX IDX_3B978F9FF6BD1646 (site_id), INDEX IDX_3B978F9F806F0F5C (equipement_id), INDEX IDX_3B978F9FB03A8386 (created_by_id), INDEX IDX_3B978F9F896DBBDE (updated_by_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE site (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, adress VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, contact VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, actif TINYINT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, organisation_id INT DEFAULT NULL, INDEX IDX_694309E49E6B1585 (organisation_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, roles JSON NOT NULL, password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, firstname VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, organisation_id INT DEFAULT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), INDEX IDX_8D93D6499E6B1585 (organisation_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE equipement ADD CONSTRAINT `FK_B8B4C6F39E6B1585` FOREIGN KEY (organisation_id) REFERENCES organisation (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE equipement ADD CONSTRAINT `FK_B8B4C6F3F6BD1646` FOREIGN KEY (site_id) REFERENCES site (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE intervention ADD CONSTRAINT `FK_D11814AB427EB8A5` FOREIGN KEY (request_id) REFERENCES request (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE intervention ADD CONSTRAINT `FK_D11814AB896DBBDE` FOREIGN KEY (updated_by_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE intervention ADD CONSTRAINT `FK_D11814AB9E6B1585` FOREIGN KEY (organisation_id) REFERENCES organisation (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE intervention ADD CONSTRAINT `FK_D11814ABB03A8386` FOREIGN KEY (created_by_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE intervention ADD CONSTRAINT `FK_D11814ABE6C5D496` FOREIGN KEY (technician_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE request ADD CONSTRAINT `FK_3B978F9F806F0F5C` FOREIGN KEY (equipement_id) REFERENCES equipement (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE request ADD CONSTRAINT `FK_3B978F9F896DBBDE` FOREIGN KEY (updated_by_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE request ADD CONSTRAINT `FK_3B978F9F9E6B1585` FOREIGN KEY (organisation_id) REFERENCES organisation (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE request ADD CONSTRAINT `FK_3B978F9FB03A8386` FOREIGN KEY (created_by_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE request ADD CONSTRAINT `FK_3B978F9FF6BD1646` FOREIGN KEY (site_id) REFERENCES site (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE site ADD CONSTRAINT `FK_694309E49E6B1585` FOREIGN KEY (organisation_id) REFERENCES organisation (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT `FK_8D93D6499E6B1585` FOREIGN KEY (organisation_id) REFERENCES organisation (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE organisation DROP actif, CHANGE nom name VARCHAR(255) NOT NULL');
+    }
+}
---
+++ src/Entity/Organisation.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrganisationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrganisationRepository::class)]
+class Organisation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?bool $actif = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    /**
+     * @var Collection<int, User>
+     */
+    #[ORM\OneToMany(targetEntity: User::class, mappedBy: 'organisation')]
+    private Collection $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function isActif(): ?bool
+    {
+        return $this->actif;
+    }
+
+    public function setActif(bool $actif): static
+    {
+        $this->actif = $actif;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): static
+    {
+        if (!$this->users->contains($user)) {
+            $this->users->add($user);
+            $user->setOrganisation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): static
+    {
+        if ($this->users->removeElement($user)) {
+            // set the owning side to null (unless already changed)
+            if ($user->getOrganisation() === $this) {
+                $user->setOrganisation(null);
+            }
+        }
+
+        return $this;
+    }
+}
